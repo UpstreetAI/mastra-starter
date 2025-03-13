811eee6c95fb7f69a1ad2a525ccedd0977d0cfc0
Avaer Kazmer <ultimus@gmail.com>
2025-03-13 15:53:30 -0700

    Add dynamic servers loading support

--- src/mastra/agents/index.ts
+++ src/mastra/agents/index.ts
@@ -1,24 +1,48 @@
 import fs from 'fs';
+import path from 'path';
 import { openai } from '@ai-sdk/openai';
 import { Agent } from '@mastra/core/agent';
 import dedent from 'dedent';
 import { MCPConfiguration } from "@mastra/mcp";
+import yaml from 'yaml';
+
+//
+
+const pnpmLockYamlString = await fs.promises.readFile(path.join('..', '..', 'pnpm-lock.yaml'), 'utf8');
+const pnpmLock = yaml.parse(pnpmLockYamlString);
+const dependencies = pnpmLock.importers['.'].dependencies as Record<string, any>;
+const specifiersToPackageNamesMap = new Map<string, string>();
+for (const [packageName, packageInfo] of Object.entries(dependencies)) {
+  if (packageInfo.specifier) {
+    specifiersToPackageNamesMap.set(packageInfo.specifier, packageName);
+  }
+}
+
+//
 
 const characterJsonPath = process.env.CHARACTER_JSON_PATH as string;
 const characterJsonString = await fs.promises.readFile(characterJsonPath, 'utf8');
 const characterJson = JSON.parse(characterJsonString);
+const { plugins = [] } = characterJson;
 
+const servers: Record<string, any> = {};
+for (const plugin of plugins) {
+  // find the package name matching this specifier
+  const pluginSpecifier = `github:${plugin}`;
+  const packageName = specifiersToPackageNamesMap.get(pluginSpecifier);
+  if (!packageName) {
+    throw new Error(`Package name not found for specifier: ${pluginSpecifier}`);
+  }
+
+  const packagePath = import.meta.resolve(packageName).replace('file://', '');
+  servers[plugin] = {
+    command: "node",
+    args: [packagePath],
+    env: process.env as any,
+  };
+}
 const mcp = new MCPConfiguration({
-  servers: {
-    // telegram: {
-    //   command: "node",
-    //   args: ["path/mcp-communicator-telegram/build/index.js"], //please add the correct path
-    //   env: {
-    //     TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN!,
-    //     CHAT_ID: process.env.CHAT_ID!,
-    //   },
-    // },
-  },
+  servers,
 });
 
 export const characterAgent = new Agent({
