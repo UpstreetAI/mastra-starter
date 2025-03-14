eb8ad782d87ad1023ff437dd19b0e12806c85c07
Avaer Kazmer <ultimus@gmail.com>
2025-03-13 18:47:35 -0700

    Add initial composio integration support

--- src/mastra/agents/index.ts
+++ src/mastra/agents/index.ts
@@ -1,20 +1,22 @@
 import fs from 'fs';
 import path from 'path';
 import { openai } from '@ai-sdk/openai';
-import { Agent } from '@mastra/core/agent';
+import { Agent, ToolsInput } from '@mastra/core/agent';
 import dedent from 'dedent';
 import { MCPConfiguration } from "@mastra/mcp";
+import { ComposioIntegration } from '@mastra/composio';
 import { PnpmPackageLookup } from 'pnpm-package-lookup';
 
+// character
 const packageLookup = new PnpmPackageLookup({
   pnpmLockYamlPath: path.join('..', '..', 'pnpm-lock.yaml'),
 });
-
 const characterJsonPath = process.env._CHARACTER_JSON_PATH as string;
 const characterJsonString = await fs.promises.readFile(characterJsonPath, 'utf8');
 const characterJson = JSON.parse(characterJsonString);
 const { plugins = [] } = characterJson;
 
+// plugins
 const servers: Record<string, any> = {};
 for (const plugin of plugins) {
   // find the package name matching this specifier
@@ -30,9 +32,32 @@ for (const plugin of plugins) {
     env: process.env as any,
   };
 }
+
+// mcp
 const mcp = new MCPConfiguration({
   servers,
 });
+const mcpTools = await mcp.getTools();
+// console.log('mcpTools', mcpTools);
+
+// composio
+const composio = new ComposioIntegration({
+  config: {
+    API_KEY: process.env.COMPOSIO_API_KEY!,
+    entityId: 'default',
+    // connectedAccountId: '899144e5-a466-428b-8a00-7c931fb57f9f',
+    connectedAccountId: '4d79004e-320a-4dc9-be1a-1037a6fe9866',
+  },
+});
+const actionsEnums = [
+  'GITHUB_STAR_A_REPOSITORY_FOR_THE_AUTHENTICATED_USER',
+  'GITHUB_ACTIVITY_LIST_STARGAZERS_FOR_REPO',
+  'GITHUB_GET_OCTOCAT',
+];
+const composioToolset = await composio.getTools({
+  actions: actionsEnums,
+}) as ToolsInput;
+// console.log('composioToolset', composioToolset);
 
 export const characterAgent = new Agent({
   name: "Character",
@@ -40,5 +65,8 @@ export const characterAgent = new Agent({
     You are the following character:
   ` + '\n' + JSON.stringify(characterJson, null, 2),
   model: openai("gpt-4o"),
-  tools: { ...(await mcp.getTools()) },
+  tools: { // ToolsInput = string -> ToolAction
+    ...mcpTools,
+    ...composioToolset,
+  },
 });
